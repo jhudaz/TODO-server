95fc7c35733c8adca1f9e152d817336e31ef361f
Jaime Velez <jandres.vr@hotmail.com>
2019-02-09 10:52:11 -0500

    more changes

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 const user = require('../api/users');
-//const todo = require('../api/todos');
+const todo = require('../api/todos');
 const chai = require('chai');
-//const should = chai.should();
+const should = chai.should();
 const chaiHttp = require('chai-http');
 const models = require('../models');
 
 chai.use(chaiHttp);
+
 const name = 'lorem'
 const email = 'prueba@prueba.com';
 const password = '12345';
